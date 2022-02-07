1ae3a78665db4855591ef07b784a731043d7fe6c
Afonso <afonso.rbn@gmail.com>
2022-02-06 22:37:28 -0200

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/persistence/PluginInstaller.java
@@ -0,0 +1,23 @@
+package br.com.uoutec.community.ediacaran.persistence;
+
+import br.com.uoutec.community.ediacaran.AbstractPlugin;
+
+public class PluginInstaller 
+	extends AbstractPlugin {
+
+	public static final String PACKAGE 					= "community";
+
+	public static final String PROVIDER 				= "ediacaran";
+
+	public static final String PLUGIN 					= "persistence";
+	
+	
+	public void install() throws Throwable{
+	}
+
+
+	@Override
+	public void uninstall() throws Throwable {
+	}
+	
+}
