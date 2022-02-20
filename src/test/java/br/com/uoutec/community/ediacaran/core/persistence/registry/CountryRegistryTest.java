70f359e4a26bf01f9e752912802cdddbf7b23703
Afonso <afonso.rbn@gmail.com>
2022-02-20 12:31:58 -0300

    #

--- src/test/java/br/com/uoutec/community/ediacaran/core/persistence/registry/CountryRegistryTest.java
+++ src/test/java/br/com/uoutec/community/ediacaran/core/persistence/registry/CountryRegistryTest.java
@@ -21,7 +21,7 @@ import br.com.uoutec.community.ediacaran.test.PluginContext;
 	@ApplicationConfigParameterTest(paramName="plugins_config_path", paramValue="ediacaran/test/br/com/uoutec/community/ediacaran/core/persistence/registry/countryregistrytest/config"),
 	@ApplicationConfigParameterTest(paramName="security_plugins_path", paramValue="ediacaran/test/br/com/uoutec/community/ediacaran/core/persistence/registry/countryregistrytest/config"),
 	@ApplicationConfigParameterTest(paramName="app_policy_path", paramValue="ediacaran/test/br/com/uoutec/community/ediacaran/core/persistence/registry/countryregistrytest/run.policy"),
-	@ApplicationConfigParameterTest(paramName="plugins_path", paramValue="ediacaran/test/countryregistrytest/plugins"),
+	@ApplicationConfigParameterTest(paramName="plugins_path", paramValue="ediacaran/test/br/com/uoutec/community/ediacaran/core/persistence/registry/countryregistrytest/plugins"),
 	@ApplicationConfigParameterTest(paramName="default", paramValue="classpath:META-INF/ediacaran-test.properties"),
 	@ApplicationConfigParameterTest(paramName="logger", paramValue="classpath:META-INF/log4j.configuration"),
 	@ApplicationConfigParameterTest(paramName="path", paramValue="../../../ediacaran-base-test")
@@ -37,75 +37,53 @@ public class CountryRegistryTest {
 		clearData();
 		
 		languageRegistry.registerLanguage(
-				new Language(
-						99, 
-						"pt", 
-						"por", 
-						"Português", 
-						"Portuguese"
-				)
+				Language.builder()
+					.withIso6391("pt")
+					.withIso6392t("por")
+					.withName("Português")
+					.withIsoName("Portuguese")
+					.build()
 		);
 		languageRegistry.flush();
 		
 		regionRegistry.registerRegion(
-				new Region(
-						99, 
-						0, 
-						"América do Sul", 
-						new Language(
-								1, 
-								null, 
-								null, 
-								null, 
-								null
-						), 
-						null
-				)
+				Region.builder()
+					.withName("América do Sul")
+					.withLanguage(Language.builder().withId(1).build())
+					.build()
 		);
 		
 		regionRegistry.flush();
 		
 		countryRegistry.registerCountry(
-				new Country(
-						1, 
-						"Brasil", 
-						0, 
-						1, 
-						"BR", 
-						"BRA", 
-						".com.br", 
-						"BRL", 
-						new Language(
-								1, 
-								null, 
-								null, 
-								null, 
-								null
-						), 
-						new Region(
-								1, 
-								0, 
-								null, 
-								null, 
-								null
-						)
-				)
+				Country.builder()
+					.withName("Brasil")
+					.withUni(1)
+					.withIsoAlpha2("BR")
+					.withIsoAlpha3("BRA")
+					.withTld(".com.br")
+					.withIso4217("BRL")
+					.withLanguage(Language.builder().withId(1).build())
+					.withRegion(Region.builder().withId(1).build())
+					.build()
 		);
+		
 		countryRegistry.flush();
 		
 		Country country = countryRegistry.getCountry(1);
-		
+
 		Assert.assertNotNull(country);
 		Assert.assertEquals(1, country.getId());
 		Assert.assertEquals("Brasil", country.getName());
-		Assert.assertEquals(0, country.getUni());
-		Assert.assertEquals(1, country.getUfi());
+		Assert.assertEquals(0, country.getUfi());
+		Assert.assertEquals(1, country.getUni());
 		Assert.assertEquals("BR", country.getIsoAlpha2());
 		Assert.assertEquals("BRA", country.getIsoAlpha3());
 		Assert.assertEquals(".com.br", country.getTld());
 		Assert.assertEquals("BRL", country.getIso4217());
 		Assert.assertEquals(1, country.getLanguage().getId());
 		Assert.assertEquals(1, country.getRegion().getId());
+
 	}
 	
 }
