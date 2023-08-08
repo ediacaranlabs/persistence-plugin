a3d834a7bbf508f6c9345a7018f4d3f5c69a2379
Brandao <Brandao@git.uoutec.com.br>
2023-08-08 20:27:42 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/persistence/entityaccess/jpa/JPAInitializer.java
+++ src/main/java/br/com/uoutec/community/ediacaran/persistence/entityaccess/jpa/JPAInitializer.java
@@ -1,122 +1,27 @@
 package br.com.uoutec.community.ediacaran.persistence.entityaccess.jpa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.spi.PersistenceUnitInfo;
-import javax.sql.DataSource;
 
-import org.brandao.brutos.annotation.scanner.DefaultScanner;
-import org.brandao.brutos.annotation.scanner.filter.AnnotationTypeFilter;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import br.com.uoutec.community.ediacaran.ResourceRegistry;
-import br.com.uoutec.community.ediacaran.persistence.Constants;
-import br.com.uoutec.community.ediacaran.plugins.PluginConfiguration;
 import br.com.uoutec.community.ediacaran.plugins.PluginType;
 
 @Singleton
 public class JPAInitializer {
 
-	private static final Logger logger = LoggerFactory.getLogger(JPAInitializer.class);
-	
-	private EntityManager entityManager;
+	@Inject
+	private JPAInitializerGlobal jpaig;
 	
 	public void close(@Disposes EntityManager entityManager) {
-		entityManager.close();
-		if(logger.isTraceEnabled()) {
-			logger.trace("Entity manager closed: {}" , entityManager);
-		}
+		jpaig.close(entityManager);
 	}
 
 	@Produces
 	@Singleton
-	@SuppressWarnings("unchecked")
 	public EntityManager createSessionFactory(PluginType pluginData) throws Throwable {
-
-		if(entityManager != null) {
-			return entityManager;
-		}
-		
-		if(logger.isTraceEnabled()) {
-			logger.trace("Creating new session factory...");
-		}
-
-		PluginConfiguration config = pluginData.getConfiguration();
-		
-		Object jtaDataSource = null;
-		Object dataSource = null;
-		
-		String dataSourceName = config.getString(Constants.JTA_DATA_SOURCE);
-		
-		if(dataSourceName != null && dataSourceName.trim().length() != 0) {
-			ResourceRegistry resourceRegistry = new ResourceRegistry();
-			jtaDataSource = resourceRegistry.lookup(dataSourceName);
-		}
-
-		dataSourceName = config.getString(Constants.DATA_SOURCE);
-		
-		if(dataSourceName != null && dataSourceName.trim().length() != 0) {
-			ResourceRegistry resourceRegistry = new ResourceRegistry();
-			dataSource = resourceRegistry.lookup(dataSourceName);
-		}
-		
-		AnnotationTypeFilter filter = new AnnotationTypeFilter();
-		
-		filter.setExpression(Arrays
-				.asList("javax.persistence.Entity"));
-
-		List<Package> packages = new ArrayList<Package>();
-		packages.addAll(Arrays.asList(pluginData.getPackages()));
-		packages.addAll(Arrays.asList(pluginData.getParentPackages()));
-		
-		String[] packageNames = new String[packages.size()];
-		
-		int i=0;
-		for(Package p: packages) {
-			packageNames[i++] = p.getName();
-		}
-
-		if(logger.isTraceEnabled()) {
-			logger.trace("Packages: {}", packages);
-			logger.trace("filter: {}", filter);
-		}
-		
-		DefaultScanner s = new DefaultScanner();
-		s.setBasePackage(packageNames);
-		s.addIncludeFilter(filter);
-		s.scan();
-		List<Class<?>> list = s.getClassList();
-		List<String> managedClass = list.stream().map(e->e.getName()).collect(Collectors.toList());
-		
-		if(logger.isTraceEnabled()) {
-			for(String clazz: managedClass) {
-				logger.trace("Found entity: {}", clazz);
-			}
-		}
-		
-		PersistenceUnitInfo pui = 
-				new PersistenceUnitInfoImp(config, 
-						"default", 
-						managedClass, 
-						(DataSource)jtaDataSource, 
-						(DataSource)dataSource);
-		
-		EntityManagerFactory emf = 
-				new HibernatePersistenceProvider()
-				.createContainerEntityManagerFactory(pui, new HashMap<Object,Object>());
-		
-		return emf.createEntityManager();
+		return jpaig.createSessionFactory(pluginData);
 	}
 
 }
